77b791d5c6a0a932756c41f540a229f0b132883e
m9soed <85126583+m9soed@users.noreply.github.com>
2024-06-03 19:52:49 +0300

    Update 2.py

--- hw13/3/2.py
+++ hw13/3/2.py
@@ -1,28 +1,45 @@
+# Импортируем необходимые модули из PySide6 и pyqtgraph
 from PySide6.QtWidgets import QApplication, QMainWindow
 import pyqtgraph as pg
 import pandas as pd
 
+# Определяем класс основного окна приложения, наследуемый от QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        # Создаем объект графика
         plot = pg.plot()
 
+        # Читаем данные из CSV-файла 'hurricanes.csv'
         df = pd.read_csv('hurricanes.csv')
+
+        # Инициализируем списки для хранения лет и суммарных значений по каждому году
         years = []
         nums = []
+
+        # Перебираем все столбцы, начиная со второго, чтобы извлечь данные по годам
         for i in df.columns[2:]:
-            years.append(i)
-            nums.append(sum(df[i].values))
-            print(years, nums)
-        x = [i+1 for i in range(len(nums))]
-        bargraph = pg.BarGraphItem(x=x, height=nums, width=0.6, brush ='r')
+            years.append(i)  # Добавляем год в список years
+            nums.append(sum(df[i].values))  # Суммируем значения и добавляем их в список nums
+            print(years, nums)  # Печатаем текущие значения списков years и nums для отладки
+
+        # Создаем список номеров для оси X
+        x = [i + 1 for i in range(len(nums))]
+
+        # Создаем объект столбчатого графика и добавляем его на график
+        bargraph = pg.BarGraphItem(x=x, height=nums, width=0.6, brush='r')
         plot.addItem(bargraph)
+
+        # Устанавливаем график в качестве центрального виджета основного окна
         self.setCentralWidget(plot)
+
+        # Настраиваем ось X для отображения меток с годами
         ax = plot.getAxis('bottom')
         ticks = [list(zip(x, years))]
         ax.setTicks(ticks)
 
-if __name__=='__main__':
+# Запускаем приложение
+if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
     window.show()
